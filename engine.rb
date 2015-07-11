a6c784658e168e9512692e9331b81b8c93c355f8
Ken Ang <angkendrick@gmail.com>
2015-07-11 19:11:52 +0000

    completed engine

---
+++ engine.rb
@@ -0,0 +1,51 @@
+class Engine
+
+  attr_accessor :random1, :random2, :answer, :operation
+
+  def initialize
+    @random1 = 0
+    @random2 = 0
+    @answer = 0
+    @operation = ""
+  end
+
+  def generate_random_numbers
+    @random1 = rand(1..20)
+    @random2 = rand(1..20)
+  end
+
+  def generate_operation
+    case rand(1..4)
+    when 1
+      @operation = "+"
+    when 2
+      @operation = "-"
+    when 3
+      @operation = "*"
+    when 4
+      @operation = "/"
+    end
+  end
+
+  def generate_answer
+    @answer = @random1.send(@operation, @random2)
+    @answer
+  end
+
+  def populate
+    generate_random_numbers
+    generate_operation
+    generate_answer
+    "#{@random1} #{@operation} #{@random2} = #{@answer}"
+  end
+
+  def check_answer(user)
+    if @answer == user
+      true
+    else
+      false
+    end
+  end
+
+
+end
---
+++ player.rb
@@ -0,0 +1,13 @@
+class Player
+
+  attr_accessor :points, :life
+
+  def initialize(name)
+    @name = name
+    @points = 0
+    @life = 3
+  end
+
+  
+
+end
